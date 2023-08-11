4bf1c7d4392c90b796bfd1ffd13ae0c8bb6d7890
Jinesh2526 <141109330+Jinesh2526@users.noreply.github.com>
2023-08-11 11:30:03 +0530

    1 file modified

--- src/app/heros/heros.component.ts
+++ src/app/heros/heros.component.ts
@@ -1,29 +1,24 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import {Hero} from '../hero';
-import {HEROES} from '../mock-heroes';
+import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 
 @Component({
-  standalone:true,
+  standalone: true,
   selector: 'app-heros',
   templateUrl: './heros.component.html',
   styleUrls: ['./heros.component.css'],
-  imports: [CommonModule,FormsModule]
+  imports: [CommonModule, FormsModule],
 })
 export class HerosComponent implements OnInit {
   // hero = 'Jinesh Shah';
-    hero:Hero ={
-  id:1,
-  name:"Jinesh Shah"
+  hero: Hero = {
+    id: 1,
+    name: 'Jinesh Shah',
+  };
+  heroes = HEROES;
+  constructor() {}
 
-
-
-  }
-  heroes=HEROES;
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
