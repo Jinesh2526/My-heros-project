512093ac2d95640ca32d1b4bede8696a12c2ab47
Jinesh2526 <141109330+Jinesh2526@users.noreply.github.com>
2023-08-11 11:44:52 +0530

    4 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
+import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { HerosComponent } from './heros/heros.component';
+import { HEROES } from './mock-heroes';
 
 @Component({
   standalone:true,
   selector: 'app-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  imports:[HerosComponent]
+  imports:[HerosComponent,CommonModule]
 })
 export class AppComponent implements OnInit {
 //  username:string="hii"
@@ -14,6 +16,7 @@ export class AppComponent implements OnInit {
 //  age:number=21
 //  photo:string="https://static.theprint.in/wp-content/uploads/2022/07/rs7-1024x477.jpg?compress=true"
 title = 'Tour of Heroes';
+heros=HEROES;
   constructor() { }
 
   ngOnInit() {
--- src/app/heros/heros.component.ts
+++ src/app/heros/heros.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Hero } from '../hero';
 import { HEROES } from '../mock-heroes';
@@ -12,12 +12,8 @@ import { HEROES } from '../mock-heroes';
   imports: [CommonModule, FormsModule],
 })
 export class HerosComponent implements OnInit {
-  // hero = 'Jinesh Shah';
-  hero: Hero = {
-    id: 1,
-    name: 'Jinesh Shah',
-  };
-  heroes = HEROES;
+
+  @Input() hero!:Hero;
   constructor() {}
 
   ngOnInit() {}
