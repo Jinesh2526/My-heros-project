0662a5c4178fdece3f2bfc1070b94673ad80cbb8
Jinesh2526 <141109330+Jinesh2526@users.noreply.github.com>
2023-08-11 11:12:23 +0530

    2 files modified, 1 added

--- src/app/heros/heros.component.ts
+++ src/app/heros/heros.component.ts
@@ -1,18 +1,22 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import{Hero} from '../hero';
+import { FormsModule } from '@angular/forms';
+import {Hero} from '../hero';
+import {HEROES} from '../mock-heroes';
+
 @Component({
   standalone:true,
   selector: 'app-heros',
   templateUrl: './heros.component.html',
   styleUrls: ['./heros.component.css'],
-  imports:[CommonModule]
+  imports: [CommonModule,FormsModule]
 })
 export class HerosComponent implements OnInit {
   // hero = 'Jinesh Shah';
-  hero:Hero ={
-id:1,
-name:"Jinesh Shah"
+    hero:Hero ={
+  id:1,
+  name:"Jinesh Shah"
+  // heroes=HEROES;
 
 
   }
---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,10 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 12, name: 'Hello' },
+  { id: 22, name: 'Chetak' },
+  { id: 32, name: 'Honda' },
+  { id: 42, name: 'Audi' },
+  { id: 52, name: ' BMW' },
+  { id: 62, name: 'HII' },
+];
