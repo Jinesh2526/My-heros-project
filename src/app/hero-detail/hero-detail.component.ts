205aaf3ea681d8ac103539654ed29122eebf3b35
Jinesh2526 <141109330+Jinesh2526@users.noreply.github.com>
2023-08-12 10:08:41 +0530

    3 files modified

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,16 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Hero } from '../hero';
 
 @Component({
-  standalone:true,
+  standalone: true,
   selector: 'app-hero-detail',
   templateUrl: './hero-detail.component.html',
-  styleUrls: ['./hero-detail.component.css']
+  styleUrls: ['./hero-detail.component.css'],
+  imports: [CommonModule, FormsModule],
 })
 export class HeroDetailComponent implements OnInit {
+  @Input() hero?: Hero;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -8,8 +8,8 @@ export const HEROES: Hero[] = [
   { id: 52, name: ' BMW' },
   { id: 62, name: 'HII' },
   { id: 43, name: ' BMW' },
-  { id: 33, name: 'HII' },
+  { id: 33, name: 'Mahindar' },
   { id: 32, name: 'Honda' },
-  { id: 42, name: 'Audi' },
-  { id: 52, name: ' BMW' },
+  { id: 42, name: 'Supra' },
+  { id: 52, name: ' Lab' },
 ];
